3cfb138eb1d81aa7759f4b84522c36f67706c0ba
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-06 16:02:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,12 +10,12 @@ array.sort do |a, b|
 end 
 end
 
-def sort_array_char_count(array, index, destination_index)
+def sort_array_char_count(array)
   array.sort do |a, b|
     a.length <=> b.length
   end
 end
 
-# def swap_elements(array)
-#   swap_elements_from_to(["a", "b", "c"],0,2)
-# end
+def swap_elements(arra, index, destination_index)
+  swap_elements_from_to(["a", "b", "c"],0,2)
+end
